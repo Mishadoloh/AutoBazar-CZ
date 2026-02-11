9e2a2351e3abf25c8914626ffbb2c1269021474d
Mykhailo Doloh <dologmihajlo31@gmail.com>
2026-02-11 20:56:07 +0200

    add

--- README.md
+++ README.md
@@ -6,8 +6,6 @@
 
 This contains everything you need to run your app locally.
 
-View your app in AI Studio: https://ai.studio/apps/drive/199iKn8M5MKtKUJJbKdTBbXFjPorDYbCo
-
 ## Run Locally
 
 **Prerequisites:**  Node.js
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Базовий шлях має бути назвою вашого репозиторію
-  base: '/AutoBazar-CZ/',
+  // Встановлюємо базу як відносний шлях для гнучкості в різних середовищах
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
